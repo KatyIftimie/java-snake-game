e76a17d94c64809c2e9aced6b160c65cd18ba780
AlexandruOriean <alexandru.oriean@gmail.com>
2020-05-25 15:21:42 +0300

    Added second snake, power-up and enemies graphics

--- src/main/java/com/codecool/snake/Main.java
+++ src/main/java/com/codecool/snake/Main.java
@@ -2,6 +2,8 @@ package com.codecool.snake;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,7 +15,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Game game = new Game();
+
+        Button restartButton = new Button("Restart");
+        restartButton.setPrefSize(100, 30);
+
+
         Scene mainScene = new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT);
+
+
+
 
         primaryStage.setTitle("Snake Game");
         primaryStage.setScene(mainScene);
